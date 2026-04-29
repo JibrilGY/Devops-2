084813dc9ee9ec085f9e128b24e9d4718ffaec4f
JibrilGY <gokayyilmaz67@hotmail.com>
2026-04-29 21:08:59 +0300

    Update: v2 message for cloud and local sync test

--- src/main/java/org/example/devops2/controller/HomeController.java
+++ src/main/java/org/example/devops2/controller/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
     @GetMapping("/")
     public ResponseEntity<String> home() {
         LOGGER.info("Home endpoint accessed");
-        return new ResponseEntity<>("Welcome to  Driver Management System API!", HttpStatus.OK);
+        return new ResponseEntity<>("Welcome to  Driver Management System API! test", HttpStatus.OK);
     }
 
     // Projenin durumunu kontrol etmek için bir health check endpoint'i
